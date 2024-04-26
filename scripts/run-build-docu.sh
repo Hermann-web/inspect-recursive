224b98f22ef646421b869d28368fc483f759eaf0
hermann-web <hermannagossou6@gmail.com>
2024-04-26 09:49:05 +0100

    Update run-build-docu.sh

--- scripts/run-build-docu.sh
+++ scripts/run-build-docu.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 poetry export -f requirements.txt --output ./docs/requirements.txt --without-hashes --with buildthedocs
+
+# sphinx-apidoc ./statanalysis/ -o ./docs/source/statanalysis/ -f -E
+
 cd docs
 make html
 cd ..
+open docs/build/html/index.html
